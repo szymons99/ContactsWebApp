fada4755a88bbc0459e712b48d539536d94ee503
Szymon Stefański <stefanski0369@gmail.com>
2023-06-24 20:08:04 +0200

    add delete option

--- ContactsWebApp/contact/urls.py
+++ ContactsWebApp/contact/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add-contact/', views.addContact, name='add-contact')
+    path('add-contact/', views.addContact, name='add-contact'),
+    path('profile/<str:pk>', views.contactProfile, name='profile'),
+    path('edit-contact/<str:pk>', views.editContact, name='edit-contact'),
+    path('delete-contact/<str:pk>', views.deleteContact, name='delete-contact')
 ]
--- ContactsWebApp/contact/views.py
+++ ContactsWebApp/contact/views.py
@@ -3,7 +3,8 @@ from .models import Contact
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    contacts = Contact.objects.all()
+    return render(request, 'index.html', {'contacts': contacts})
 
 def addContact(request):
     if request.method == "POST":
@@ -16,5 +17,31 @@ def addContact(request):
         )
         new_contact.save()
         return redirect('/')
-    
+
     return render(request, 'new.html')
+
+def contactProfile(requset, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(requset, 'contact-profile.html', {'contact': contact})
+
+def editContact(request, pk):
+    contact = Contact.objects.get(id=pk)
+
+    if request.method == "POST":
+        contact.full_name == request.POST['fullname']
+        contact.relationship == request.POST['relationship']
+        contact.email == request.POST['email']
+        contact.phone_number == request.POST['phone-number']
+        contact.address == request.POST['address']
+        contact.save()
+
+        return redirect('/profile/'+str(contact.id))
+    return render(request, 'edit.html', {'contact': contact})
+
+def deleteContact(request, pk):
+    contact = Contact.objects.get(id=pk)
+
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('/')
+    return render(request, "delete.html", {'contact': contact})
