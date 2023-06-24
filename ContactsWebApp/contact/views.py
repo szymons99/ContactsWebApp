24f88c3a5a0c1fafc5d0b4cd3b3fa9879c3173f1
Szymon Stefański <stefanski0369@gmail.com>
2023-06-24 20:22:29 +0200

    add search function and fix edit

--- ContactsWebApp/contact/views.py
+++ ContactsWebApp/contact/views.py
@@ -4,7 +4,13 @@ from .models import Contact
 # Create your views here.
 def index(request):
     contacts = Contact.objects.all()
-    return render(request, 'index.html', {'contacts': contacts})
+    search_input = request.GET.get('search-area')
+    if search_input:
+        contacts = Contact.objects.filter(full_name__icontains=search_input)
+    else:
+        contacts = Contact.objects.all()
+        search_input = ''
+    return render(request, 'index.html', {'contacts': contacts, 'searchinput': search_input})
 
 def addContact(request):
     if request.method == "POST":
@@ -27,12 +33,12 @@ def contactProfile(requset, pk):
 def editContact(request, pk):
     contact = Contact.objects.get(id=pk)
 
-    if request.method == "POST":
-        contact.full_name == request.POST['fullname']
-        contact.relationship == request.POST['relationship']
-        contact.email == request.POST['email']
-        contact.phone_number == request.POST['phone-number']
-        contact.address == request.POST['address']
+    if request.method == 'POST':
+        contact.full_name = request.POST['fullname']
+        contact.relationship = request.POST['relationship']
+        contact.email = request.POST['email']
+        contact.phone_number = request.POST['phone-number']
+        contact.address = request.POST['address']
         contact.save()
 
         return redirect('/profile/'+str(contact.id))
